de82d610e51e6126813bcc7e6a1faf08183a6063
var-sama <vava.and.aa72@gmail.com>
2026-04-14 12:30:25 +0700

    database

---
+++ app/core/database.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+require_once '/../config/app.php';
+
+class Database
+{
+protected $connection;
+
+    public function __construct()
+    {
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    }
+
+    if(!$this->connection){
+        die("Error");
+    }
+}
+
+?>
